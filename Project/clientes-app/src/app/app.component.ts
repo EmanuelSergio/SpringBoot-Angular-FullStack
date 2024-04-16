ed1b80820c6f1161589f7c3298855fe3b3ad31a7
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2024-04-16 14:16:44 -0300

    função de abrir a sideBar e criando footer

--- Project/clientes-app/src/app/app.component.ts
+++ Project/clientes-app/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import jQuery  from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'clientes-app';
+export class AppComponent implements AfterViewInit {
+
+
+  ngAfterViewInit(): void {
+
+    (function ($) {
+      'use strict';
+
+      // Add active state to sidbar nav links
+      var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+      $('#layoutSidenav_nav .sb-sidenav a.nav-link').each(function () {
+        if (this instanceof HTMLAnchorElement && this.href === path) {
+          $(this).addClass('active');
+        }
+      });
+
+      // Toggle the side navigation
+      $('#sidebarToggle').on('click', function (e) {
+        e.preventDefault();
+        $('body').toggleClass('sb-sidenav-toggled');
+      });
+    })(jQuery);
+
+  }
+
 }
--- Project/clientes-app/src/app/app.module.ts
+++ Project/clientes-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
