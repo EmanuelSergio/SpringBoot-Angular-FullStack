abdad289a5c95047646c69892cc7e91fd3c36c88
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2024-04-17 00:43:54 -0300

    2 way data-binding

--- Project/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ Project/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Cliente } from '../cliente';
+
 
 @Component({
   selector: 'app-clientes-form',
   templateUrl: './clientes-form.component.html',
-  styleUrl: './clientes-form.component.css'
+  styleUrl: './clientes-form.component.css',
 })
-export class ClientesFormComponent {
+export class ClientesFormComponent implements OnInit {
+
+
+  cliente: Cliente;
+  nome: string = 'fulano'
+
+  constructor(){
+    this.cliente = new Cliente();
+    this.cliente.nome = "eeee"
+  }
+
+
+  ngOnInit(): void {}
+
+  clicar(){
+    console.log("apertei");
+
+  }
+
+
 
 }
