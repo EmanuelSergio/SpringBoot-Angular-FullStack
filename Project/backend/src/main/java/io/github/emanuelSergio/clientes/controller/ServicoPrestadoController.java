892e3736d88d0f2eb6e51f459769b5ec8cdae34e
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2024-04-19 15:10:20 -0300

    pesquisando servico por mes ou nome

--- Project/backend/src/main/java/io/github/emanuelSergio/clientes/controller/ServicoPrestadoController.java
+++ Project/backend/src/main/java/io/github/emanuelSergio/clientes/controller/ServicoPrestadoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -32,21 +33,24 @@ public class ServicoPrestadoController {
     @ResponseStatus(HttpStatus.CREATED)
     public ServicoPrestado salvar(@RequestBody ServicoPrestadoDTO dto){
         LocalDate data = LocalDate.parse(dto.getData(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-
         Integer idCliente = dto.getIdCliente();
-
         Cliente cliente = clienteRepository
                 .findById(idCliente)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cliente inesistente"));
-
-
-
         ServicoPrestado servicoPrestado = new ServicoPrestado();
         servicoPrestado.setDescricao(dto.getDescricao());
         servicoPrestado.setData(data);
         servicoPrestado.setCliente(cliente);
         servicoPrestado.setValor( bigDecimalConverter.converter(dto.getPreco()));
         return repository.save(servicoPrestado);
+    }
+
+    @GetMapping("")
+    public List<ServicoPrestado>pesquisar(
+            @RequestParam(value = "nome", required = false, defaultValue = "") String nome,
+            @RequestParam(value = "mes", required = false)Integer mes){
+
+        return repository.findByNomeClienteAndMes("%" + nome + "%", mes);
     }
 
 
---
+++ Project/clientes-app/src/app/servico-prestado/servico-prestado.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ServicoPrestadoRoutingModule } from './servico-prestado-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ServicoPrestadoRoutingModule
+  ]
+})
+export class ServicoPrestadoModule { }
