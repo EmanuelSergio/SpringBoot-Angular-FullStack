a516177eb6532fb6e67f63bda0bdb9833a07a8e7
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2024-04-22 11:10:11 -0300

    criando pagina de serviço

--- Project/clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ Project/clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../../clientes/cliente';
+import { ClientesService } from '../../clientes.service';
 
 @Component({
   selector: 'app-servico-prestado-form',
   templateUrl: './servico-prestado-form.component.html',
   styleUrl: './servico-prestado-form.component.css'
 })
-export class ServicoPrestadoFormComponent {
+export class ServicoPrestadoFormComponent implements OnInit {
+
+  clientes: Cliente[] = [];
+
+  constructor(private clienteService: ClientesService){}
+
+  ngOnInit(): void {
+    this.clienteService.getClientes().subscribe(response => this.clientes = response)
+  }
+
+  onSubmit(){
+    return console.log("enviado");
+
+  }
 
 }
