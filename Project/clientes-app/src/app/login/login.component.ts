39f16e15dd6eda59bc4b8e923ba60c3764c127c4
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2024-04-25 11:10:08 -0300

    tela de login completa

--- Project/clientes-app/src/app/login/login.component.ts
+++ Project/clientes-app/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +11,27 @@ export class LoginComponent {
   username?:string;
   password?:string;
   loginError?: boolean;
+  cadastrando?: boolean;
 
-  constructor(){
+  constructor(private router: Router){
+
   }
 
   onSubmit(){
     console.log(`User: ${this.username}, Pass: ${this.password}`);
+    this.router.navigate(['/home']);
 
   }
 
+  preparandoCadastrar(event: Event){
+    event.preventDefault();//metodo q n deixa nada acontecer com a view
+    this.cadastrando = true
+    console.log(this.cadastrando);
+
+  }
+
+  cancelarCadastrar(){
+    this.cadastrando = false;
+  }
 
 }
